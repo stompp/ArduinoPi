ca0a6f88f1ad5f0bfd70d8f2c2b59f89bddb7d82
stompp <josemiguel.tomejimenez@gmail.com>
2023-04-10 02:27:17 +0200

    Added arduino platforms common includes and defines for ESP boards

--- ArduinoPlatforms.h
+++ ArduinoPlatforms.h
@@ -2,7 +2,19 @@
 #define _ARDUINOPLATFORMS_H_
 
 #ifdef ARDUINO
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
 #include <Arduino.h>
+#if defined(ESP8266) || defined(ESP32)
+// interrupt handlers and related code must be in RAM
+// ICACHE_RAM_ATTR is deprecated
+#define ISR_ATTR IRAM_ATTR
+#else
+#define ISR_ATTR
+#endif
+#endif
+
 #elif defined(RPI)
 #include "ArduinoPi.h"
 #endif
